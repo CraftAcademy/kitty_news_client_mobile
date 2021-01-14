b0b302e390ec512810353900b2b0e66ec21dea23
viktor <viktorkarlsson125@gmail.com>
2021-01-14 16:43:33 +0100

    refactoring position of style for consistency

--- components/DisplayArticlesList.jsx
+++ components/DisplayArticlesList.jsx
@@ -1,35 +1,32 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image, Dimensions } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
 
 const DisplayArticlesList = ({ article }) => {
   return (
     <>
       <Image
         source={{ uri: article.image }}
-        defaultSource={{ uri: "https://thumbs.dreamstime.com/b/no-image-available-icon-vector-illustration-flat-design-140476186.jpg" }}
+        defaultSource={{
+          uri:
+            "https://thumbs.dreamstime.com/b/no-image-available-icon-vector-illustration-flat-design-140476186.jpg",
+        }}
         style={styles.image}
       />
       <View style={styles.card}>
-        <Text style={styles.title}>
-          {article.title}
-        </Text>
-        <Text style={styles.lead}>
-          {article.lead}
-        </Text>
-        <Text style={styles.created}>
-          {article.created}
-        </Text>
+        <Text style={styles.title}>{article.title}</Text>
+        <Text style={styles.lead}>{article.lead}</Text>
+        <Text style={styles.created}>{article.created}</Text>
       </View>
     </>
-  )
-}
+  );
+};
 
-export default DisplayArticlesList
+export default DisplayArticlesList;
 
 const styles = StyleSheet.create({
   card: {
     position: "absolute",
-    width: Dimensions.get('window').width,
+    width: Dimensions.get("window").width,
     paddingLeft: 10,
     paddingRight: 10,
     bottom: 0,
@@ -47,10 +44,10 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 12,
     paddingBottom: 5,
-    paddingTop: 3
+    paddingTop: 3,
   },
   image: {
     height: 250,
-    width: Dimensions.get('window').width,
+    width: Dimensions.get("window").width,
   },
-})
+});
